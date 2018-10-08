61fe524f307d82accbf048ed078836ae47490eba
juanmaone <juanmafunes@hotmail.com>
2018-10-08 19:21:02 -0300

    1

---
+++ PROYECTO 2 HOME BANKING/scripts/script.js
@@ -0,0 +1,37 @@
+//PROYECTO HOME BANKING
+//OCTUBRE 2018
+//HOME BANKING
+
+//DECLARACIÓN DE VARIABLES
+
+var nombreUsuario='juanma';
+var saldoCuenta = 250000;
+var limiteExtraccion=20000;
+
+//Funciones
+/*extraerDinero
+depositarDinero
+pagarServicio
+transferirDinero
+cambiarLimiteDeExtraccion
+*/
+
+function extraerDinero(){
+
+}
+
+function depositarDinero(){
+
+}
+
+function pagarServicio(){
+
+}
+
+function transferirDinero(){
+
+}
+
+function cambiarLimiteDeExtraccion(){
+
+}
